d01d9e4afa6a356e6211bfef23e9722408df9013
kissmiklosjr <kissmiklosjr@gmail.com>
2021-12-04 08:35:12 +0100

    parse input

--- 2021/day04/part1.js
+++ 2021/day04/part1.js
@@ -3,6 +3,13 @@ const fetchInput = require("../fetchInput")
 
 const main = async () =>{
     const input = await fetchInput(4)
+    const bingoNumbers = input.shift().split(",").map(x=>Number(x))
+    const boards = []
+    input.shift()
+    for (i=0; i < input.length; i+=6) {
+        boards.push(input.slice(i, i+5).map(x=>x.split(" ").filter(Boolean).map(x=>Number(x))))
+    }
+    console.log(boards, bingoNumbers)
 
 }
 
