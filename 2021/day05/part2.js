6e61638090d14c957088b05db489ad201e04c154
Miklos Kiss <kissmiklosjr@gmail.com>
2021-12-05 08:48:22 +0100

    make it more readable

--- 2021/day05/part2.js
+++ 2021/day05/part2.js
@@ -2,6 +2,7 @@ const fetchInput = require("../fetchInput");
 
 const main = async () => {
   const input = await fetchInput(5);
+  // parse input
   const coords = input.map((line) => {
     const splittedLine = line.split("->").filter(Boolean);
     const from = splittedLine[0];
@@ -11,6 +12,8 @@ const main = async () => {
       to.split(",").map((x) => Number(x)),
     ];
   });
+
+  //find size of matrix
   const xCoords = [];
   const yCoords = [];
   coords.forEach((e) => {
@@ -23,46 +26,48 @@ const main = async () => {
   const xMax = Math.max(...xCoords) + 1;
   const yMax = Math.max(...yCoords) + 1;
 
+  //initialize the given size of matrix with zeros
   const result = new Array(yMax).fill(0).map(() => new Array(xMax).fill(0));
-  coords.forEach((pair) => {
-    const from = pair[0];
-    const to = pair[1];
-    if (from[1] == to[1]) {
-      if (from[0] < to[0]) {
-        for (i = from[0]; i <= to[0]; i++) {
-          result[from[1]][i] += 1;
+
+  coords.forEach((coord) => {
+    let [x1, y1] = coord[0];
+    let [x2, y2] = coord[1];
+    if (y1 == y2) {
+      if (x1 < x2) {
+        for (i = x1; i <= x2; i++) {
+          result[y1][i] += 1;
         }
-      } else if (from[0] > to[0]) {
-        for (i = from[0]; i >= to[0]; i--) {
-          result[from[1]][i] += 1;
+      } else if (x1 > x2) {
+        for (i = x1; i >= x2; i--) {
+          result[y1][i] += 1;
         }
       }
-    } else if (from[0] == to[0]) {
-      if (from[1] < to[1]) {
-        for (i = from[1]; i <= to[1]; i++) {
-          result[i][from[0]] += 1;
+    } else if (x1 == x2) {
+      if (y1 < y2) {
+        for (i = y1; i <= y2; i++) {
+          result[i][x1] += 1;
         }
-      } else if (from[1] > to[1]) {
-        for (i = from[1]; i >= to[1]; i--) {
-          result[i][from[0]] += 1;
+      } else if (y1 > y2) {
+        for (i = y1; i >= y2; i--) {
+          result[i][x1] += 1;
         }
       }
     } else {
-      //
-      if (from[0] < to[0] && from[1] < to[1]) {
-        for (i = from[0], j = from[1]; i <= to[0]; i++, j++) {
+      //diagonal
+      if (x1 < x2 && y1 < y2) {
+        for (i = x1, j = y1; i <= x2; i++, j++) {
           result[j][i] += 1;
         }
-      } else if (from[0] > to[0] && from[1] > to[1]) {
-        for (i = from[0], j = from[1]; i >= to[0]; i--, j--) {
+      } else if (x1 > x2 && y1 > y2) {
+        for (i = x1, j = y1; i >= x2; i--, j--) {
           result[j][i] += 1;
         }
-      } else if (from[0] > to[0] && from[1] < to[1]) {
-        for (i = from[0], j = from[1]; i >= to[0]; i--, j++) {
+      } else if (x1 > x2 && y1 < y2) {
+        for (i = x1, j = y1; i >= x2; i--, j++) {
           result[j][i] += 1;
         }
-      } else if (from[0] < to[0] && from[1] > to[1]) {
-        for (i = from[0], j = from[1]; i <= to[0]; i++, j--) {
+      } else if (x1 < x2 && y1 > y2) {
+        for (i = x1, j = y1; i <= x2; i++, j--) {
           result[j][i] += 1;
         }
       }
