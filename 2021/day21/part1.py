decc057b4ccc59fb719f00ea50bd324ad6ff1a72
Miklos Kiss <kissmiklosjr@gmail.com>
2021-12-21 08:41:17 +0100

    day21 solution

--- 2021/day21/part1.py
+++ 2021/day21/part1.py
@@ -1,15 +1,28 @@
-def print_grid(grid):
-    print("----------------------")
-    for r in grid:
-        print(" ".join(r))
+from collections import deque
 
 
 def main():
-    matrix = []
-    with open('input.txt') as matrix:
-        matrix = [[x for x in y] for y in matrix.read().split("\n")]
+    p1 = 6
+    p2 = 8
+    pos = [p1, p2]
+    scores = [0, 0]
+    turn = 0
+    rolls = 0
+    q = deque(range(1, 101))
 
-    print(matrix)
+    while scores[0] < 1000 and scores[1] < 1000:
+        value = sum(list(q)[:3])
+        q.rotate(-3)
+
+        current_pos = ((value + pos[turn]) % 10)
+        if current_pos == 0:
+            current_pos = 10
+        scores[turn] += current_pos
+        pos[turn] = current_pos
+
+        rolls += 3
+        turn = 1 - turn
+    print("RESULT: ", min(scores) * rolls)
 
 
 if __name__ == "__main__":
---
+++ 2021/day21/part2.py
@@ -0,0 +1,42 @@
+from itertools import product
+from functools import lru_cache
+
+cache = {}
+
+
+@lru_cache(None)
+def roll(scores, pos, turn):
+    wins = [0, 0]
+    current_pos = [x for x in pos]
+    current_scores = [x for x in scores]
+
+    for first, second, third in product((1, 2, 3), (1, 2, 3), (1, 2, 3)):
+        value = first + second + third
+
+        current_pos[turn] = ((value + pos[turn]) % 10)
+        if current_pos[turn] == 0:
+            current_pos[turn] = 10
+
+        current_scores[turn] = scores[turn] + current_pos[turn]
+
+        if current_scores[turn] >= 21:
+            wins[turn] += 1
+        else:
+            p1, p2 = roll((current_scores[0], current_scores[1]),
+                          (current_pos[0], current_pos[1]), 1 - turn)
+            wins[0] += p1
+            wins[1] += p2
+
+    return wins
+
+
+def main():
+    p1 = 6
+    p2 = 8
+    result = roll((0, 0), (p1, p2), 0)
+
+    print("RESULT: ", max(result))
+
+
+if __name__ == "__main__":
+    main()
